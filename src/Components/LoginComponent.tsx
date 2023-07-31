9876156bd8eed1b0b4a0f10b719ed3a42925159f
DSHINDE21 <dinesh.s.shinde1@gmail.com>
2023-07-31 15:05:16 +0530

    Added-Input-Validations

--- src/Components/LoginComponent.tsx
+++ src/Components/LoginComponent.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FormValues, LoginFormProps } from "../Models/LoginModel";
+import { Link, useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage, FormikHelpers } from "formik";
+import * as Yup from "yup";
 import {
   Center,
   Container,
@@ -10,40 +14,50 @@ import {
   useToast,
   Text,
 } from "@chakra-ui/react";
-import { LoginFormProps } from "../Models/LoginModel";
-import { Link, useNavigate } from "react-router-dom";
 
 const LoginComponent: React.FC<LoginFormProps> = ({ onLogin }) => {
-  const [Email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
   const navigate = useNavigate();
   const toast = useToast();
-  // const [loggedIn, setLoggedIn] = useState(false);
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+  // Validation schema using Yup
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(6, "Password must be at least 6 characters long"),
+  });
 
-    // Check if the provided Email and password match the specified values
-    if (Email === "dinesh.s.shinde1@gmail.com" && password === "admin") {
-      toast({
-        title: "Login Successful",
-        description: "Welcome back!",
-        status: "success",
-        duration: 1000,
-        isClosable: true,
-      });
-      onLogin(Email, password);
-      navigate("/list");
-      // setLoggedIn(true);
-    } else {
-      toast({
-        title: "Login Failed",
-        description: "Invalid Email or password. Please try again.",
-        status: "error",
-        duration: 1000,
-        isClosable: true,
-      });
-    }
+  const handleSubmit = (
+    values: FormValues,
+    { setSubmitting }: FormikHelpers<FormValues>
+  ) => {
+    setTimeout(() => {
+      if (
+        values.email === "dinesh.s.shinde1@gmail.com" &&
+        values.password === "admin1234"
+      ) {
+        toast({
+          title: "Login Successful",
+          description: "Welcome back!",
+          status: "success",
+          duration: 1000,
+          isClosable: true,
+        });
+        onLogin(values.email, values.password);
+        navigate("/list");
+      } else {
+        toast({
+          title: "Login Failed",
+          description: "Invalid Email or password. Please try again.",
+          status: "error",
+          duration: 1000,
+          isClosable: true,
+        });
+      }
+      setSubmitting(false);
+    }, 1000);
   };
 
   return (
@@ -59,37 +73,63 @@ const LoginComponent: React.FC<LoginFormProps> = ({ onLogin }) => {
         p="20px"
         borderRadius="10px"
       >
-        <form onSubmit={handleSubmit}>
-          <Heading as="h1" size="lg" mb={6} textAlign="center" color="white">
-            Login Page
-          </Heading>
-          <FormControl mb={4} isRequired>
-            <FormLabel htmlFor="Email">Email:</FormLabel>
-            <Input
-              type="email"
-              id="Email"
-              value={Email}
-              placeholder="enter Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
-          <FormControl mb={4}>
-            <FormLabel htmlFor="password">Password:</FormLabel>
-            <Input
-              type="password"
-              id="password"
-              value={password}
-              placeholder="enter password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-          <Button type="submit" colorScheme="teal" size="lg" w="100%">
-            Login
-          </Button>
-          <Text p={3}>
-            Don't have an account? <Link to="/register">Register</Link>
-          </Text>
-        </form>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Heading
+                as="h1"
+                size="lg"
+                mb={6}
+                textAlign="center"
+                color="white"
+              >
+                Login Page
+              </Heading>
+              <FormControl mb={4}>
+                <FormLabel htmlFor="email">Email:</FormLabel>
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  as={Input}
+                  placeholder="Enter Email"
+                />
+                <ErrorMessage name="email" component="div" className="error" />
+              </FormControl>
+              <FormControl mb={4}>
+                <FormLabel htmlFor="password">Password:</FormLabel>
+                <Field
+                  type="password"
+                  id="password"
+                  name="password"
+                  as={Input}
+                  placeholder="Enter password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="error"
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                colorScheme="teal"
+                size="lg"
+                w="100%"
+                isLoading={isSubmitting}
+              >
+                Login
+              </Button>
+              <Text p={3}>
+                Don't have an account? <Link to="/register">Register</Link>
+              </Text>
+            </Form>
+          )}
+        </Formik>
       </Container>
     </Center>
   );
--- src/Models/LoginModel.ts
+++ src/Models/LoginModel.ts
@@ -2,6 +2,11 @@ export interface LoginFormProps {
   onLogin: (email: string, password: string) => void;
 }
 
+export interface FormValues {
+  email: string;
+  password: string;
+}
+
 export interface LogoutFormProps {
   onLogout: (email: string, password: string) => null;
 }
